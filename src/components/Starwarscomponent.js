8ab7bb4b80b173717e3898c70dc1d667f9a5ce07
Felipe <fpizarro.ux@gmail.com>
2022-08-11 18:31:01 +0200

    ternary operator for title display

--- src/components/Starwarscomponent.js
+++ src/components/Starwarscomponent.js
@@ -28,22 +28,34 @@ function Starwarscomponent() {
     <button className='btn btn-outline-light m-2' onClick={characters}>Characters</button>
     <button className='btn btn-outline-light m-2' onClick={planets}>Planets</button>
     <button className='btn btn-outline-light m-2' onClick={starships}>Starships</button>
-    <div>{update}</div>
-    <div className='card bg-transparent'>
+    <div>
+      {update === "people" ? <h3>Name and height of some SW characters</h3> : null}
+      {update === "planets" ? <h3>Name and population of some SW planets</h3> : null}
+      {update === "starships" ? <h3>Name and passengers capacity of some SW starships</h3> : null}
+      </div>
+    <div className='container'>
+      <div className='row g-2'>
     {content.map((item, i) =>{
       return (
-        <p key={i}>  
-
+        <div className='card col-4 bg-transparent' key={i}>  
+          <p className='mt-2'>
             {JSON.stringify(item.name + ",")} 
-            {update ==="people" ? " Height:"+ JSON.stringify(item.height) : null }
-            {update ==="planets" ?  " Population:" + JSON.stringify(item.population) : null }
-            {update ==="starships" ? " Passengers:" + JSON.stringify(item.passengers) : null }
+
             
-        </p>
+          </p>
+          <p>
+
+                {update ==="people" ? " Height:"+ JSON.stringify(item.height) : null }
+                {update ==="planets" ?  " Population:" + JSON.stringify(item.population) : null }
+                {update ==="starships" ? " Passengers:" + JSON.stringify(item.passengers) : null }
+                
+            </p>
+        </div>
       )
     })}
 
-
+      </div>
+      <h6>Done with SW API</h6>
     </div>
       
     </>
